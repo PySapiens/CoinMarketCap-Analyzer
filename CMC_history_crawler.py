e940a0d1eb5af1231b441574270d5e09c12b3a53
PySapiens <35865797+PySapiens@users.noreply.github.com>
2018-01-30 23:02:12 +0100

    Update CMC_history_crawler.py

--- CMC_history_crawler.py
+++ CMC_history_crawler.py
@@ -7,92 +7,65 @@ import csv
 
 def cmc_crawler():
     my_url = "https://coinmarketcap.com/historical/20130428/"
-    uClient = uReq(my_url) #downloads the website
-    page_html = uClient.read() #html of the website
-    uClient.close() #closing client
-
-    page_soup = soup(page_html, "html.parser") #html parser
+    uClient = uReq(my_url)  # downloads the website
+    page_html = uClient.read()  # html of the website
+    uClient.close()  # closing client
+    page_soup = soup(page_html, "html.parser")  # html parser
 
     exchanges_data = {}
     counter = 0
 
-    #get exchanges and links
+    #link to next week
+    paginator = page_soup.select(".pagination")[0]
+    link_nw = paginator.findAll('a', href=True)[1].attrs["href"]
 
-    table = page_soup.find('table')
-    table_body = table.find('tbody')
+    #go to next week
+    ck_uClient = uReq(link_nw)  # downloads the website
+    ck_page_html = ck_uClient.read()  # html of the website
+    ck_uClient.close()  # closing client
 
-    rows = table_body.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        number = cols[0].text
-        name = cols[1].text
-        symbol = cols[2].text
-        mcap = cols[3].text
-        price = cols[4].text
-        circ_sup = cols[5].text
-        
-        
-        
-        
-     
-    
-    
-        
-  #while my_url nicht empty     
-  my_url = page_soup.findAll("li", text=Next Week)
-    
-    uClient = uReq(my_url) #downloads the website
-    page_html = uClient.read() #html of the website
-    uClient.close() #closing client
-    
-    
-    
-    
-    
-    
-    
-    
-    for header in page_soup.select(".volume-header"):
-        counter = counter + 1
-        if counter <= 3: #limitiert auf nummer an exchanges!!!
-            exchange_name = header.select_one("a").text
-            exchange_url = urljoin(my_url,header.select_one("a").attrs["href"])
-        else:
-            break
-        print(exchange_name)
-        print(exchange_url)
+    ck_page_soup = soup(ck_page_html, "html.parser")  # html parser
 
-        #go to exchange url
-        uClient_exchange = uReq(exchange_url)
-        page_html_exchange = uClient_exchange.read()
-        uClient_exchange.close()
+    #link to next next week
+    ck_paginator = ck_page_soup.select(".pagination")[0]
+    ck_link_nw = ck_paginator.findAll('a', href=True)[1].attrs["href"]
 
-        page_soup_exchange = soup(page_html_exchange, "html.parser")
+    #if there us a next next week, prepare for loop
+    if ck_link_nw == '':
+        break
+    else:
+        uClient = uReq(my_url)  # downloads the website
+        page_html = uClient.read()  # html of the website
+        uClient.close()  # closing client
+        page_soup = soup(page_html, "html.parser")  # html parser
 
-        pairs_volumes = []
-        table = page_soup_exchange.find('table')
+
+        date = page_soup.find_all('h1')[1].text.split("- ")[1]
+        table = page_soup.find('table')
         table_body = table.find('tbody')
-
         rows = table_body.find_all('tr')
+        max_crcs = 100
+        counter = 0
         for row in rows:
             cols = row.find_all('td')
-            pair = cols[2].text.split("/") #cols[2].text
-            volume = row.select_one(".volume").attrs["data-usd"]
+            number = cols[0].text
+            name = cols[1].text
+            symbol = cols[2].text
+            mcap = cols[3].text
+            price = cols[4].text
+            circ_sup = cols[5].text
 
-            pairs_volumes.append([pair, volume])
+    #url auf nächste seite ändern
+    paginator = page_soup.select(".pagination")[0]
+    my_url = paginator.findAll('a', href=True)[1].attrs["href"]
 
-        exchanges_data[exchange_name] = pairs_volumes
 
-    print(exchanges_data)
-    
-    return exchanges_data
 
 def csv_writer(dict):
     with open('cmc_exchanges.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for key, value in dict.items():
             writer.writerow([key, value])
-
 
 #csv_writer(cmc_crawler())
